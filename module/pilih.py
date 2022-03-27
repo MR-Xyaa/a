ac97c4dee4688d3cc41c141d3dfdf48c50b3767b
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 17:40:22 +0700

    Update pilih.py

--- module/pilih.py
+++ module/pilih.py
@@ -7,20 +7,20 @@ def mengetik(s):
         sys.stdout.flush()
         time.sleep(random.random() * 0.1)
 
-echo $cyan"[===============================================[>"
-echo $green "1.> Install Bahan By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $blue "2.> Tampilan Termux Oh-My-Zsh By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $purple "3.> MTK By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $green "4.> DMBF By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $blue "5.> Gatau By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $purple "6.> Pribadi By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $red "00.> EXIT MY PROGRAM
-echo $cyan"[+]===============================================[+]"
-echo "\033[32;1m"
+mengetik('[===============================================[>')
+mengetik('  1.> Install Bahan By MR-Xyaa')
+mengetik('[===============================================[>')
+mengetik('  2.> Tampilan Termux Oh-My-Zsh By MR-Xyaa')
+mengetik('[===============================================[>')
+mengetik('  3.> MTK By MR-Xyaa')
+mengetik('[===============================================[>')
+mengetik('  4.> DMBF By MR-Xyaa')
+mengetik('[===============================================[>')
+mengetik('  5.> Gatau By MR-Xyaa')
+mengetik('[===============================================[>')
+mengetik('  6.> Pribadi By MR-Xyaa')
+mengetik('[===============================================[>')
+mengetik('  00.> EXIT MY PROGRAM')
+mengetik('[+]===============================================[+]')
+mengetik('Silakan Pilih)
 read -p "root@MR-Xyaa~#" bro
