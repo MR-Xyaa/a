9d215ae1e628991463a680ebc14118e71727a9ce
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 13:35:00 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,10 +1,14 @@
 from optparse import Option
 print("Selamat Datang Di Dukun Online")
 print("Pilih Pertanyaan :")
-print("1.Sisa Umur Saya?")
-print("2.Kurangi Umur?")
-print("3.Menambah Umur?")
+print("1.SIAPA PACARKU?")
+print("2.BERAPA UMURKU?")
+print("3.NAMAKU?")
 option=int(input("Pilih Pertanyaan : "))
 
 input=1 
-print("ah")
+print("APRILLIA SURYATIMAH")
+input=2
+print("17 TAHUN")
+input=3
+print("IBNU UMAR SYAID")
