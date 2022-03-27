b8994e125d8a60d866e10aaae82672dfe519c061
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 13:44:43 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,21 +1,8 @@
 import os, sys
 from optparse import Option
 print("Selamat Datang Di Dukun Online")
-print("Pilih Pertanyaan :")
-print("1.SIAPA PACARKU?")
-print("2.BERAPA UMURKU?")
-print("3.NAMAKU?")
-option=int(input("Pilih Pertanyaan : "))
-
-input=1 
-print("APRILLIA SURYATIMAH")
-else: 
-print("Yang Bener Kontol")
-input=2
-print("17 TAHUN")
-input=3
-print("IBNU UMAR SYAID")
-
-os.system('clear')
-
+print("Tekan Enter Untuk Lanjut")
+print("Atau")
+print("CTRL+C Untuk Keluar")
+option=int(input("pilih:"))
 
