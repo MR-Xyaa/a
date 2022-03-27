acd496f4c221011a22625b70f1660248340acc21
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 17:29:05 +0700

    Update and rename pilih.sh to logo.sh

---
+++ module/logo.sh
@@ -0,0 +1,24 @@
+#!/system/xbin/bash
+
+clear
+
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+
+
+toilet -f big -F gay ToolsV1 | lolcat
+echo $yellow"[◇]=============================================[◇]"
+echo $green      "[•] Author__: MR-Xyaa"                         
+echo $red        "[•] FB______: Xyaa Xyaa"                         
+echo $purple     "[•] GitHub__: github.com/MR-Xyaa"              
+echo $yellow"[◇]=============================================[◇]"
+echo""
+echo""
+
+
