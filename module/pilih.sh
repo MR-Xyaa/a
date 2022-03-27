ab0c164f7c0fc19dae38609d7a28b75b24fe10eb
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 17:23:05 +0700

    Update pilih.sh

--- module/pilih.sh
+++ module/pilih.sh
@@ -21,23 +21,6 @@ echo $yellow"[◇]=============================================[◇]"
 echo""
 echo""
 
-echo $cyan"[===============================================[>"
-echo $green "1.> Install Bahan By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $blue "2.> Tampilan Termux Oh-My-Zsh By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $purple "3.> MTK By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $green "4.> DMBF By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $blue "5.> Gatau By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $purple "6.> Pribadi By MR-Xyaa"
-echo $cyan"[===============================================[>"
-echo $red "00.> EXIT MY PROGRAM
-echo $cyan"[+]===============================================[+]"
-echo "\033[32;1m"
-read -p "root@MR-Xyaa~#" bro
 
 if [ $bro = 1 ] || [ $bro = 1 ]
 then
