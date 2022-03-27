fa601e70f5cff7d8478a9228513addbbb47092fe
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-28 04:01:45 +0700

    Create load2.py

---
+++ module/load2.py
@@ -0,0 +1,26 @@
+import itertools
+import threading
+import time
+import sys
+
+done = False
+
+#animasi loading
+def animate():
+    for c in itertools.cycle(['|', '/', '-', '\\']):
+        if done:
+            break
+        sys.stdout.write('\rMenginstall MENU MR-Xyaa... ' + c)
+        sys.stdout.flush()
+        time.sleep(0.1)
+    sys.stdout.write('\rDone!     ')
+
+t = threading.Thread(target=animate)
+t.start()
+
+#proses lama disini
+
+time.sleep(10)
+done = True
+
+
