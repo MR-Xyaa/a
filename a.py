1e2bb8b173fbf79c0e5ba360333fbd9ae00804e0
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 13:00:06 +0700

    Update a.py

--- a.py
+++ a.py
@@ -4,10 +4,10 @@ print("Pilih Pertanyaan :")
 print("1.Sisa Umur Saya?")
 print("2.Kurangi Umur?")
 print("3.Menambah Umur?")
-option=int(input("Pilih Option :"))
+option=int(input("Pilih option :"))
 
 
-	if option ==1:
+if option ==1:
 print("Sisa Umur Anda 3600 Hari Lagi")
 elif option==2 :
 print("Sisa Umur Anda 3.600 Hari Lagi Mau Dikurang Berapa?")
