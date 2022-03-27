0ca470329f33dc14895f9851f865548fe63d679d
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 13:31:18 +0700

    Update a.py

--- a.py
+++ a.py
@@ -6,36 +6,5 @@ print("2.Kurangi Umur?")
 print("3.Menambah Umur?")
 option=int(input("Pilih Pertanyaan : "))
 
-     def Option==1 :
-         print("Sisa Umur Anda 3600 Hari Lagi")
-elif option==2 :
-         print("Sisa Umur Anda 3.600 Hari Lagi Mau Dikurang Berapa?")
-print("1.  Kurangi 600 Hari")
-print("2.  Kurangi 1.000 Hari")
-umur_kamu=3.600
-option2=int=(input("Umur :")
-
-
-     if option==2 :
-umur1=umur_kamu-600
-print("Sisa Umur Kamu Adalah :",umur1)
-
-elif option2==2 :
-umur2=umur_kamu-1.000
-print("Sisa Umur Kamu Adalah :",umur2)
-
-
-else:
-         print"(Pilih Yang Bener Kontol!")
-
-elif option==3:
-umur_kamu=3.600
-print("Umur Kamu Tinggal 3.600 Hari Mau Nambah Berapa?")
-
-option3=int(input("Masukan Jumlah Umur Kamu :"))
-umur3=umur_kamu+option3
-
-print("Sisa Umur :",umur3)
-
-else:
-         print("Pilih Yang Bener Anjing!")
+input=1 
+print("ah")
