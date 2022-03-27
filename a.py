c7170b9a972f0affc79eff72422238b9363ad64c
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 14:08:52 +0700

    Update a.py

--- a.py
+++ a.py
@@ -1,45 +1,9 @@
-import os, sys
-os.system('clear')
 from optparse import Option
-print("×WELLCOME TO TOOLS MR XYAA:)")
-print("×Tekan Enter Untuk Lanjut")
-print("×Atau")
-print("×CTRL+C Untuk Keluar")
-option=int(input("pilih="))
-os.system('python2 a.py')
-os.system('clear')
-import os, sys
-print ("\033[1;32mMasukan UserName&Password Dulu Ya:)")
-print ("\033[1;31;1mKalo Gak Tau Kunjungi Github MR-Xyaa")
-print ("\033[1;32mhttps://github.com/MR-Xyaa")
-username = 'MR'      
-password = 'Xyaa'
+print("Wellcome To Tools MR-Xyaa")
+print("Pilih :")
+print("1.Lanjut")
+print("2.Keluar")
+option=int(input("Lanju Apa Keluar : "))
 
-def restart():
-	ngulang = sys.executable
-	os.execl(ngulang, ngulang, *sys.argv)
-
-def main():
-	uname = raw_input("username : ")
-	if uname == username:
-		pwd = raw_input("password : ")
-
-		if pwd == password:
-			print "\n\033[1;34mHello Welcome To Tools MR-Xyaa", 
-			sys.exit()
-
-		else:
-			print "\n\033[1;36mSorry Invalid Password !!!\033[00m"
-			print "Back Login\n"
-			restart()
-
-	else:
-		print "\n\033[1;36mSorry Invalid Username !!!\033[00m"
-		print "Back Login\n"
-		restart()
-
-try:
-	main()
-except KeyboardInterrupt:
-	os.system('clear')
-	restart()
+if option == 1:
+print("lanjut")
