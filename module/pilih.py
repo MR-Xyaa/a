577f6598a66e009b2b712a0bebb77b11c0e299b0
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 17:27:53 +0700

    Create pilih.py

---
+++ module/pilih.py
@@ -0,0 +1,26 @@
+import random
+import sys
+import time
+def mengetik(s):
+    for c in s + '\n':
+        sys.stdout.write(c)
+        sys.stdout.flush()
+        time.sleep(random.random() * 0.1)
+
+echo $cyan"[===============================================[>"
+echo $green "1.> Install Bahan By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $blue "2.> Tampilan Termux Oh-My-Zsh By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $purple "3.> MTK By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $green "4.> DMBF By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $blue "5.> Gatau By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $purple "6.> Pribadi By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $red "00.> EXIT MY PROGRAM
+echo $cyan"[+]===============================================[+]"
+echo "\033[32;1m"
+read -p "root@MR-Xyaa~#" bro
