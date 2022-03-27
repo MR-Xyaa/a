044f79436a16d7ae4cdd71d5686284ae9440c227
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 14:16:54 +0700

    Update a.py

--- a.py
+++ a.py
@@ -5,9 +5,19 @@ from optparse import Option
 print("Wellcome To Tools MR-Xyaa")
 print("Pilih :")
 print("1.Lanjut")
-print("2.Keluar")
+print("99.Keluar")
 option=int(input("Lanju Apa Keluar : "))
 
-def clearscreen():
-if sys.platform == 1:
-Print("lanjut")
+
+def backtomenu_option():
+	print(backtomenu_banner)
+	backtomenu = input("santet > ")
+	
+	if backtomenu == "99":
+		restart_program()
+	elif backtomenu == "00":
+		sys.exit()
+	else:
+		print("\nERROR: Wrong input")
+		time.sleep(2)
+		restart_program()
