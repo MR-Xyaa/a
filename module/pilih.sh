5a00a024f11fc4bbca676b8fb1df226528aa38a2
MR-Xyaa <102247436+MR-Xyaa@users.noreply.github.com>
2022-03-27 16:34:16 +0700

    Create pilih.sh

---
+++ module/pilih.sh
@@ -0,0 +1,147 @@
+#!/system/xbin/bash
+
+clear
+
+
+blue='\033[34;1m'
+green='\033[32;1m'  
+purple='\033[35;1m'
+cyan='\033[36;1m'
+red='\033[31;1m'
+white='\033[37;1m'                                           
+yellow='\033[33;1m'
+sleep 1
+echo $red
+echo ""
+
+echo $cyan"[===============================================[>"
+echo $green "1.> Install Bahan By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $blue "2.> Tampilan Termux Oh-My-Zsh By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $purple "3.> MTK By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $green "4.> DMBF By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $blue "5.> Gatau By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $purple "6.> Pribadi By MR-Xyaa"
+echo $cyan"[===============================================[>"
+echo $red "00.> EXIT MY PROGRAM
+read -p "root@MR-Xyaa:~#" bro
+
+if [ $bro = 1 ] || [ $bro = 1 ]
+then
+clear
+figlet "MR-Xyaa" | lolcat
+pkg update && pkg upgrade
+
+pkg install python python2 python3
+
+pip install requests mechanize
+
+pip2 install requests mechanize
+
+pkg install git
+
+pkg install ruby
+
+pkg install curl
+
+gem install lolcat
+
+pip2 install php
+
+pkg install figlet
+
+pkg install wget
+
+pkg install nano
+
+pkg install bash
+
+pkg install cowsay
+
+apt update && apt upgrade -y
+
+pkg install bash
+
+pkg install pv -y
+
+pkg install wget -y
+
+pkg install git -y
+fi
+
+if [ $bro = 2 ] || [ $bro = 2 ]
+then
+clear
+figlet "MR-Xyaa" | lolcat
+git clone https://github.com/MR-Xyaa/Oh-My-Zsh
+cd Oh-My--Zsh
+git pull
+sh oh-my-zsh
+fi
+
+if [ $bro = 3 ] || [ $bro = 3 ]
+then
+clear
+figlet "MR-Xyaa" | lolcat
+git clone https://github.com/MR-Xyaa/MTK
+cd MTK
+git pull
+sh mtk.sh
+fi
+
+if [ $bro = 4 ] || [ $bro = 4 ]
+then
+clear
+figlet "MR-Xyaa" | lolcat
+git clone https://github.com/MR-Xyaa/dmbf
+cd dmbf
+git pull
+python dmbf.py
+fi
+
+
+if [ $bro = 6 ] || [ $bro = 6 ]
+then
+clear
+figlet "MR-Xyaa" | lolcat
+git clone https://github.com/MR-Xyaa/pribadi
+cd pribadi
+git pull
+sh aprillia.sh
+fi
+
+if [ $bro = 5 ] || [ $bro = 5 ]
+then
+clear
+figlet "MR-Xyaa" | lolcat
+git clone https://github.com/MR-Xyaa/belajar
+cd belajar
+git pull
+sh bljar.sh
+fi
+
+
+f [ $bro = 0 ] || [ $bro = 00 ]
+then
+echo "\033[32;1mMR-Xyaa"
+sleep 1
+echo "\033[33;1mWe Security"
+sleep 1
+echo " We Not Friends"
+sleep 1
+echo "We Are Family"
+sleep 1
+echo "Hacking Is Not Criminal;)"
+sleep 1
+echo "Ketika Sebuah Hayalan Tidak tercapai"
+sleep 1
+echo "Maka Terus lah BerJuang Dan Berusaha:)"
+sleep 1
+echo "\033[32;1mKarna Suatu Hari Nanti Kamu akan Mendapatkannya:)"
+sleep 1
+exit
+fi
